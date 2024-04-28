0816193d6eb66e560f1c18c8e93c8ad85b46ca90
Victor Nyingi Muchiri <nyingimuchiriv@gmail.com>
2024-04-28 22:16:47 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "../src/App.css";
+import Header from "./Components/Header";
+import SearchBar from "./Components/SearchBar";
+import TransactionForm from "./Components/TransactionForm";
+import TransactionTable from "./Components/TransactionTable";
+
+function App() {
+  const things = [
+    {
+      date: "2024-12-01",
+      description: "Movie",
+      category: "Entertainment",
+      Amount: "$100",
+    },
+    {
+      date: "2024-11-02",
+      description: "game",
+      category: "Gaming",
+      Amount: "$110",
+    },
+    {
+      date: "2024-10-03",
+      description: "Hicking",
+      category: "Entertainment",
+      Amount: "$120",
+    },
+    {
+      date: "2024-09-04",
+      description: "Fries",
+      category: "Food",
+      Amount: "$130",
+    },
+    {
+      date: "2024-08-05",
+      description: "Swimming",
+      category: "Entertainment",
+      Amount: "$140",
+    },
+    {
+      date: "2024-12-01",
+      description: "Check-up",
+      category: "Health",
+      Amount: "$150",
+    },
+  ];
+  const [transactions, setTransactions] = useState(things);
+  return (
+    <div className="">
+      <Header />
+      <SearchBar
+        transactions={transactions}
+        setTransactions={setTransactions}
+        things={things}
+      />
+      <TransactionForm />
+      <TransactionTable transactions={transactions} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TransactionForm.jsx
@@ -0,0 +1,22 @@
+function TransactionForm() {
+  return (
+    <div className="row border">
+      <div className="col-3">
+        <input type="date" />
+      </div>
+      <div className="col-3">
+        <input type="text" placeholder="Description" />
+      </div>
+      <div className="col-3">
+        <input type="text" placeholder="Category" />
+      </div>
+      <div className="col-3">
+        <input type="text" placeholder="Amount" />
+      </div>
+      <div className="col-12">
+        <button>Add Transaction</button>
+      </div>
+    </div>
+  );
+}
+export default TransactionForm;
